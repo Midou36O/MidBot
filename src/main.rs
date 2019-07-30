2d349156809573bf3fb91c01dae46cb78b1afecf
Midou36O <facemioo4@gmail.com>
2019-07-30 19:11:09 +0100

    ready

--- src/main.rs
+++ src/main.rs
@@ -2,40 +2,48 @@ use serenity::client::Client;
 use serenity::model::channel::Message;
 use serenity::prelude::{EventHandler, Context};
 use serenity::framework::standard::{
-	StandardFramework,
-	CommandResult,
-	macros::{
-		command,
-		group
-	}
+    StandardFramework,
+    CommandResult,
+    macros::{
+        command,
+        group
+    }
 };
 
 group!({
-	name: "general",
-	options: {},
-	commands: [ping],
+    name: "general",
+    options: {},
+    commands: [],
 });
 
 use std::env;
 
 struct Handler;
-impl EventHandler for Handler {}
+
+impl EventHandler for Handler {
+    fn message(&self, ctx: Context, msg: Message) {
+       if msg.content == "!ping" {
+            println!("Shard {}", ctx.shard_id);
+
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!") {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
 
 fn main() {
-    //Login with a bot token from the environment
-    let mut client = Client::new(&env::var("insert a discord token here").expect("token"), Handler)
-    .expect ("Error creating client");
+    // Login with a bot token from the environment
+    let mut client = Client::new(&env::var("DISCORD_TOKEN").expect("token"), Handler)
+        .expect("Error creating client");
     client.with_framework(StandardFramework::new()
-    	.configute(|c|c.prefix(">>")) //this is the bot's prefix ">>"
-    	.group(&GENERAL_GROUP));
-    //Start listening for events by starting a single shard
-    if let Err(why) = client.start(){
-    	println!("An error occured while running the client: {:?}", why);
+        .configure(|c| c.prefix(">>")) // set the bot's prefix to ">>"
+        .group(&GENERAL_GROUP));
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start() {
+        println!("An error occurred while running the client: {:?}", why);
     }
 }
-#[command]
-fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
-	msg.reply(ctx, "Pong!")?;
 
-	ok(())
-}
+#[command]
